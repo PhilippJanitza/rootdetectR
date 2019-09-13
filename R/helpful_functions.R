825cbcdde4488021ed7d0dde7d451364f0b9f6d3
Philipp Janitza <philipp.janitza@landw.uni-halle.de>
2019-09-13 17:47:34 +0200

    added new function subset_fac2 will split datasets in subsets by Factor2 - examples are still missing - would need a good data.set with multiple Factor2 - in general a better example data.set

--- R/helpful_functions.R
+++ R/helpful_functions.R
@@ -48,3 +48,49 @@ rm_outlier <- function(x, fill_na = F) {
 }
 
 
+
+#' @title Create subsets by Factor2
+#' @description This function splits Normalized Rootdetection Output in subsets for each Factor2 control and treatment pair.
+#' To be functional Factor2 must contain a control and at least two other conditions (treatments)
+#' @param root_norm data.frame; LengthMM normalized output from Rootdetection containing NO 10mm values
+#' @param control string; name of the Factor2 control condition
+#' @return list of data.frames; each subset will be in an seperated data.frame stored in a list of data.frames
+#' @example
+#' # will come in next version with an data.frame containing multiple Factor2
+#'
+#' @export
+subset_fac2 <- function(root_norm, control = '20'){
+
+  root_norm$Factor2 <- as.factor(root_norm$Factor2)
+
+  if(length(levels(root_norm$Factor2)) <= 2){
+
+    warning('There are only 2 levels of Factor2 - no subsetting by Factor2 possible!')
+
+    #else create subtables
+  } else {
+
+    # variable containing only Factor2 control
+    root_norm_control <- root_norm[root_norm$Factor2 == control,]
+
+    # create empty list to put subsets in
+    dfl <- list()
+
+
+    # loop over all levels in Factor2 that are not control
+    for (i in levels(root_norm$Factor2)[levels(root_norm$Factor2) != control]) {
+
+      # get subset for each i
+      sub <- root_norm[root_norm$Factor2 == i,]
+      #bind root_norm_control with sub_temp
+      sub_control <- rbind(sub, root_norm_control)
+
+      # paste in list and name
+      name <- paste(unique(root_norm_control$Factor2), '_and_', i, sep = '')
+      dfl[[name]] <- sub_control
+
+    }
+  }
+  return(dfl)
+}
+
